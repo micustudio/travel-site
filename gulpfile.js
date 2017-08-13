da7e5618ba4bb491013534b6e239971917573c59
Michael <micumieu@gmail.com>
2017-08-13 14:30:49 -0700

    added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,29 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     variables = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    imports = require('postcss-import');
-
-gulp.task('default', function(){
-    console.log("Default Gulp Task is running. Initialized.");
-});
+    imports = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('watch', function(){
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function(){
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     })
 })
+
+gulp.task('default', function(){
+    console.log("Default Gulp Task is running. The command is just 'gulp'.");
+});
 
 gulp.task('html', function(){
     console.log('The index.html file has been updated!');
@@ -29,3 +37,8 @@ gulp.task('styles', function(){
         .pipe(postcss([imports, variables, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 })
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
