56a4040246961ef763ee8efc715f6a83fb7de558
Michael <micumieu@gmail.com>
2017-08-13 16:30:49 -0700

    add css syntax error handling in gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ var gulp = require('gulp'),
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([imports, variables, nested, autoprefixer]))
+        .on('error', function(errorInfo) { 
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 })
