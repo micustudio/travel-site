d70c366fe33350552afbf943a800f8dc784927c8
Michael <micumieu@gmail.com>
2017-08-13 16:10:38 -0700

    organized gulp file structire

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    variables = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    imports = require('postcss-import');
+
+
+gulp.task('styles', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([imports, variables, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+})
